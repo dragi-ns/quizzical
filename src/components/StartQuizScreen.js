a8b894311b85278840a14a4231338874c7446347
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2022-07-18 12:09:50 +0200

    Add start quiz screen

---
+++ src/components/StartQuizScreen.js
@@ -0,0 +1,13 @@
+function StartQuizScreen() {
+  return (
+    <section className="sqs">
+      <h1 className="sqs--title">Quizzical</h1>
+      <p className="sqs--description">
+        Answer fun questions and test your knowledge in this trivia quiz.
+      </p>
+      <button className="sqs--button">Start quiz</button>
+    </section>
+  );
+}
+
+export default StartQuizScreen;
